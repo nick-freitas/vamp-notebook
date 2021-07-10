14376200744ddce7177b333818b0a3ae750cc09f
Nick Freitas <nickfrei@gmail.com>
2021-07-10 19:45:11 -0400

    comment out notes for demo

--- src/app/characters/components/characters/characters.component.ts
+++ src/app/characters/components/characters/characters.component.ts
@@ -1,6 +1,7 @@
 import { Component, ChangeDetectionStrategy, OnInit } from "@angular/core";
-import { StateService } from "../../../core/state/state.service";
+import { MobileService } from "../../../core/mobile.service";
 import { NoteService } from "../../note.service";
+import { SelectedViewService } from "../../selected-view.service";
 
 @Component({
   templateUrl: "./characters.component.html",
@@ -9,5 +10,9 @@ import { NoteService } from "../../note.service";
 })
 export class CharactersComponent {
   public characterListOpened: boolean;
-  constructor(public state: StateService, public noteService: NoteService) {}
+  constructor(
+    public mobileService: MobileService,
+    public noteService: NoteService,
+    public selectedViewService: SelectedViewService
+  ) {}
 }
